01a5930286529c86509f9d6f4715de44849c8e06
mi8s <miissymgni>
2025-02-21 12:29:09 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\Court_typeController;
+use App\Http\Controllers\CourtsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/MasterProject', function (){
+    return view('MasterProject');
+});
+
+Route::get('/mproject', function (){
+    return view('mproject');
+});
+
+
+Route::get('/home', function (){
+    return view('home');
+});
+
+
+Route::get('/funfact', function (){
+    return view('kevin');
+});
+
+Route::get ('/court_type', [Court_typeController::class, 'index']);
+Route::get ('/Mcourts', [CourtsController::class, 'Mcourts']);
+Route::post ('/court_type', [Court_typeController::class, 'store']);
+
+Route::resource('courts', CourtsController::class);
